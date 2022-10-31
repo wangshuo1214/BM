571f0beaee6a5b83a57818f307313957c1474890
wangshuo02 <wangshuo02@css.com.cn>
2022-10-31 18:15:14 +0800

    用户集合查询接口

--- src/main/java/com/bm/controller/BmUserController.java
+++ src/main/java/com/bm/controller/BmUserController.java
@@ -1,9 +1,12 @@
 package com.bm.controller;
 
+import com.bm.domain.BmUser;
 import com.bm.service.IBmUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -11,4 +14,12 @@ public class BmUserController {
 
     @Autowired
     private IBmUserService iBmUserService;
+
+    @RequestMapping("/userList")
+    public List<BmUser> getUserList(){
+
+        List<BmUser> bmUserList = iBmUserService.list();
+
+        return bmUserList;
+    }
 }
